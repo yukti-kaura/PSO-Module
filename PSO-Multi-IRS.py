367e5bb542b1cb8343f05cc8e32605f18f854910
Yukti Kaura <yuktikaura1@gmail.com>
2024-08-05 16:54:35 +0530

    PSO Working now

--- PSO-Multi-IRS.py
+++ PSO-Multi-IRS.py
@@ -107,7 +107,6 @@ def pso_function(parameter_values, bounds, n_particles, m_iterations, inertia, c
 
     return iteration_best_values
 
-
 M = 1  # No. of antennas on BS
 N = N1 = N2 = NB  = 8  # No. IRS elements
 K = 1  # No.of transmit antennas at the relay device
@@ -118,35 +117,10 @@ m = 1 # m=1,2,..., 2^n-1
 ####################### Reflection co-efficient matrix for IRS_1 (I1) #########################
 zeta_I1 = [random.uniform(0, 1) for _ in range(N)]
 theta_I1 = [random.uniform(0, 2*math.pi) for _ in range(N)]
-
-### Only one phase coefficient matrix is assumed as it is NOT STAR IRS ###
-phi_I1_value = [math.sqrt(zeta) * math.exp(theta) for zeta, theta in zip(zeta_I1, theta_I1)]
-
-### Generate a complex matrix ###
-phi_I1 = np.zeros((N, N), dtype=complex)
-np.fill_diagonal(phi_I1, phi_I1_value)
-
-####################### Reflection co-efficient matrix for IRS_2 (I2) #########################
 zeta_I2 = [random.uniform(0, 1) for _ in range(N)]
 theta_I2 = [random.uniform(0, 2*math.pi) for _ in range(N)]
-
-### Only one phase coefficient matrix is assumed as it is NOT STAR IRS ###
-phi_I2_value = [math.sqrt(zeta) * math.exp(theta) for zeta, theta in zip(zeta_I2, theta_I2)]
-
-### Generate a complex matrix ###
-phi_I2 = np.zeros((N, N), dtype=complex)
-np.fill_diagonal(phi_I2, phi_I2_value)
-
-####################### Reflection co-efficient matrix for IRS_B (IB) #########################
 zeta_IB = [random.uniform(0, 1) for _ in range(N)]
 theta_IB = [random.uniform(0, 2*math.pi) for _ in range(N)]
-
-### Only one phase coefficient matrix is assumed as it is NOT STAR IRS ###
-phi_IB_value = [math.sqrt(zeta) * math.exp(theta) for zeta, theta in zip(zeta_IB, theta_IB)]
-
-### Generate a complex matrix ###
-phi_IB = np.zeros((N, N), dtype=complex)
-np.fill_diagonal(phi_IB, phi_IB_value)
 
 #### Initialize PSO parameters ####
 n_particles = 30
@@ -164,7 +138,7 @@ social = 0.5
 # bounds = (min_bound, max_bound)
 
 bounds = [(0,1)]*(3*N) + [(0,2*math.pi)]*(3*N) +  [(0,0.5)]*4 + [(0,1)]*4
-print(len(bounds))
+# print(len(bounds))
 
 ### Rate Thresholds ###
 R_11_th = R_12_th = R_21_th = R_22_th = 0.05
@@ -207,10 +181,17 @@ H_1_B = H_2_B = H_11_1 = H_12_1 = H_21_2 = H_22_2 = 0.0
 SINR_B_D22 = SINR_B_D21 = SINR_B_D12 = SINR_B_D12 = SINR_R2_D22 = SINR_R2_D21 = SINR_R1_D11 = SINR_R1_D12 = 0.0
 rate_D11 =  rate_D12 = rate_D21 = rate_D22 = 0
 
+
+
 ### Check with Justin ###
 rho = 0.1
 ### We have Power and IRS Phase shifts to optimize ###
 par =  zeta_I1, zeta_I2,  zeta_IB, theta_I1,theta_I2, theta_IB, P_11, P_12, P_21,P_22, P_dash_11, P_dash_12, P_dash_21, P_dash_22
+
+def find_min(a, b):
+  if(a > b):
+    return a
+  return b
 
 def generate_channel(N, K, path_loss):
     h = (1/np.sqrt(2)) * (np.random.randn(N, K) + 1j * np.random.randn(N, K))
@@ -257,59 +238,76 @@ h_I2_1 = generate_channel(N, 1, get_pathloss_direct(D_1_I2))
 
 ## Duplexing interference ###
 h_1_1 =  generate_channel(1, 1, 1)
-
 h_2_2 =  generate_channel(1, 1, 1)
+### Only one phase coefficient matrix is assumed as it is NOT STAR IRS ###
+phi_I1_value = [math.sqrt(zeta) * math.exp(theta) for zeta, theta in zip(zeta_I1, theta_I1)]
 
+### Generate a complex matrix ###
+phi_I1 = np.zeros((N, N), dtype=complex)
+np.fill_diagonal(phi_I1, phi_I1_value)
+
+####################### Reflection co-efficient matrix for IRS_2 (I2) #########################
+
+### Only one phase coefficient matrix is assumed as it is NOT STAR IRS ###
+phi_I2_value = [math.sqrt(zeta) * math.exp(theta) for zeta, theta in zip(zeta_I2, theta_I2)]
+
+### Generate a complex matrix ###
+phi_I2 = np.zeros((N, N), dtype=complex)
+np.fill_diagonal(phi_I2, phi_I2_value)
+
+####################### Reflection co-efficient matrix for IRS_B (IB) #########################
+
+### Only one phase coefficient matrix is assumed as it is NOT STAR IRS ###
+phi_IB_value = [math.sqrt(zeta) * math.exp(theta) for zeta, theta in zip(zeta_IB, theta_IB)]
+
+### Generate a complex matrix ###
+phi_IB = np.zeros((N, N), dtype=complex)
+np.fill_diagonal(phi_IB, phi_IB_value)
 
 ### Calculate Channel Gain ###
-H_1_B = np.dot(np.dot(np.conjugate(h_IB_B).transpose(),phi_IB),h_1_IB.transpose())
-H_2_B = np.dot(np.dot(np.conjugate(h_IB_B).transpose(),phi_IB),h_2_IB.transpose())
-H_11_1 = np.dot(np.dot(np.conjugate(h_I1_1).transpose(),phi_I1),h_11_I1.transpose())
-H_12_1 = np.dot(np.dot(np.conjugate(h_I1_1).transpose(),phi_I1),h_12_I1.transpose())
-H_21_2 = np.dot(np.dot(np.conjugate(h_I2_2).transpose(),phi_I2),h_21_I2.transpose())
-H_22_2 = np.dot(np.dot(np.conjugate(h_I2_2).transpose(),phi_I2),h_22_I2.transpose())
+H_1_B = np.dot(np.dot(np.conjugate(h_IB_B).transpose(), phi_IB), h_1_IB.transpose())
+H_2_B = np.dot(np.dot(np.conjugate(h_IB_B).transpose(), phi_IB), h_2_IB.transpose())
+H_11_1 = np.dot(np.dot(np.conjugate(h_I1_1).transpose(), phi_I1), h_11_I1.transpose())
+H_12_1 = np.dot(np.dot(np.conjugate(h_I1_1).transpose(), phi_I1), h_12_I1.transpose())
+H_21_2 = np.dot(np.dot(np.conjugate(h_I2_2).transpose(), phi_I2), h_21_I2.transpose())
+H_22_2 = np.dot(np.dot(np.conjugate(h_I2_2).transpose(), phi_I2), h_22_I2.transpose())
 
 ### Interference Link Channel Gain ###
-H_2_1 = np.dot(np.dot(np.conjugate(h_IB_1).transpose(),phi_IB),h_2_IB.transpose())
-H_1_2 = np.dot(np.dot(np.conjugate(h_IB_2).transpose(),phi_IB),h_1_IB.transpose())
-H_22_1 = np.dot(np.dot(np.conjugate(h_I2_1).transpose(),phi_I2),h_22_I2.transpose())
-H_21_1 = np.dot(np.dot(np.conjugate(h_I2_1).transpose(),phi_I2),h_21_I2.transpose())
-H_12_2 = np.dot(np.dot(np.conjugate(h_I1_2).transpose(),phi_I1),h_12_I1.transpose())
-H_11_2 = np.dot(np.dot(np.conjugate(h_I1_2).transpose(),phi_I1),h_11_I1.transpose())
-
+H_2_1 = np.dot(np.dot(np.conjugate(h_IB_1).transpose(), phi_IB), h_2_IB.transpose())
+H_1_2 = np.dot(np.dot(np.conjugate(h_IB_2).transpose(), phi_IB), h_1_IB.transpose())
+H_22_1 = np.dot(np.dot(np.conjugate(h_I2_1).transpose(), phi_I2), h_22_I2.transpose())
+H_21_1 = np.dot(np.dot(np.conjugate(h_I2_1).transpose(), phi_I2), h_21_I2.transpose())
+H_12_2 = np.dot(np.dot(np.conjugate(h_I1_2).transpose(), phi_I1), h_12_I1.transpose())
+H_11_2 = np.dot(np.dot(np.conjugate(h_I1_2).transpose(), phi_I1), h_11_I1.transpose())
 
 ### Calculate SINR Values ###
 
-F_1 = (P_21*np.abs(H_22_1.item())**2) + ((P_dash_21 + P_dash_22)*np.abs(H_2_1.item())**2 + np.abs(h_1_1.item())**2*rho**2*(P_dash_11 + P_dash_12))
+F_1 = (P_21 * np.abs(H_22_1.item()) ** 2) + (
+            (P_dash_21 + P_dash_22) * np.abs(H_2_1.item()) ** 2 + np.abs(h_1_1.item()) ** 2 * rho ** 2 * (
+                P_dash_11 + P_dash_12))
 
 # SINR at R1 to detect symbol of D11 considering D12 as interference assuming H11_1 > H12_1
-SINR_R1_D11 = (P_11*np.abs (H_11_1.item())**2)/(P_12*np.abs(H_12_1.item())**2 + F_1 + sigma_1**2)
+SINR_R1_D11 = (P_11 * np.abs(H_11_1.item()) ** 2) / (P_12 * np.abs(H_12_1.item()) ** 2 + F_1 + sigma_1 ** 2)
 
 # SINR at R1 to detect symbol of D12 considering D11 as interference assuming D11 has already been decoded
-SINR_R1_D12 = (P_12*np.abs (H_12_1.item())**2)/(F_1 + sigma_1**2)
-print(SINR_R1_D12)
+SINR_R1_D12 = (P_12 * np.abs(H_12_1.item()) ** 2) / (F_1 + sigma_1 ** 2)
 
 
 # SINR at R2
-F_2 = P_11*np.abs(H_11_2.item())**2 + P_12*np.abs(H_12_2.item())**2 + (P_dash_11 + P_dash_12)*np.abs(H_1_2.item())**2 + np.abs(h_2_2.item())**2*rho**2*(P_dash_21 + P_dash_12)
+F_2 = P_11 * np.abs(H_11_2.item()) ** 2 + P_12 * np.abs(H_12_2.item()) ** 2 + (P_dash_11 + P_dash_12) * np.abs(
+    H_1_2.item()) ** 2 + np.abs(h_2_2.item()) ** 2 * rho ** 2 * (P_dash_21 + P_dash_12)
 
-SINR_R2_D21 = P_21*np.abs(H_21_2.item())**2/(P_22*np.abs(H_22_2.item())**2 + F_2 + sigma_2**2)
-SINR_R2_D22 = P_22*np.abs(H_22_2.item())**2/(F_2+sigma_2**2)
-
+SINR_R2_D21 = P_21 * np.abs(H_21_2.item()) ** 2 / (P_22 * np.abs(H_22_2.item()) ** 2 + F_2 + sigma_2 ** 2)
+SINR_R2_D22 = P_22 * np.abs(H_22_2.item()) ** 2 / (F_2 + sigma_2 ** 2)
 
 # SINR at Base Station B
-SINR_B_D11 = P_dash_11 * np.abs(H_1_B.item())**2/(P_dash_12*np.abs(H_1_B.item())**2 + (P_dash_21 + P_dash_22)*np.abs(H_2_B.item())**2 + sigma_b**2)
-SINR_B_D12 = P_dash_12 * np.abs(H_1_B.item())**2/((P_dash_21 + P_dash_22) * np.abs(H_2_B.item())**2 + sigma_b**2)
-print(SINR_B_D12)
-SINR_B_D21 = P_dash_21 * np.abs(H_2_B.item())**2/(P_dash_22)*np.abs(H_2_B.item())**2 + sigma_b**2
-SINR_B_D22 = P_dash_22 * np.abs(H_2_B.item())**2/sigma_b**2
+SINR_B_D11 = P_dash_11 * np.abs(H_1_B.item()) ** 2 / (
+            P_dash_12 * np.abs(H_1_B.item()) ** 2 + (P_dash_21 + P_dash_22) * np.abs(H_2_B.item()) ** 2 + sigma_b ** 2)
+SINR_B_D12 = P_dash_12 * np.abs(H_1_B.item()) ** 2 / (
+            (P_dash_21 + P_dash_22) * np.abs(H_2_B.item()) ** 2 + sigma_b ** 2)
 
-
-def find_min(a, b):
-  if(a > b):
-    return a
-  return b
-
+SINR_B_D21 = P_dash_21 * np.abs(H_2_B.item()) ** 2 / (P_dash_22) * np.abs(H_2_B.item()) ** 2 + sigma_b ** 2
+SINR_B_D22 = P_dash_22 * np.abs(H_2_B.item()) ** 2 / sigma_b ** 2
 
 ### Get SINR ###
 ### Rate of D11 ###
@@ -320,9 +318,96 @@ rate_D12 = math.log2(1 +  find_min(SINR_R1_D12, SINR_B_D12))
 rate_D21 = math.log2(1 +  find_min(SINR_R2_D21, SINR_B_D21))
 ### Rate of D22 ###
 rate_D22 = math.log2(1 +  find_min(SINR_R2_D22, SINR_B_D22))
+def reinit():
+    global rate_D21, rate_D11, rate_D22, rate_D12
+    ### Only one phase coefficient matrix is assumed as it is NOT STAR IRS ###
+    phi_I1_value = [math.sqrt(zeta) * math.exp(theta) for zeta, theta in zip(zeta_I1, theta_I1)]
+
+    ### Generate a complex matrix ###
+    phi_I1 = np.zeros((N, N), dtype=complex)
+    np.fill_diagonal(phi_I1, phi_I1_value)
+
+    ####################### Reflection co-efficient matrix for IRS_2 (I2) #########################
+
+    ### Only one phase coefficient matrix is assumed as it is NOT STAR IRS ###
+    phi_I2_value = [math.sqrt(zeta) * math.exp(theta) for zeta, theta in zip(zeta_I2, theta_I2)]
+
+    ### Generate a complex matrix ###
+    phi_I2 = np.zeros((N, N), dtype=complex)
+    np.fill_diagonal(phi_I2, phi_I2_value)
+
+    ####################### Reflection co-efficient matrix for IRS_B (IB) #########################
+
+    ### Only one phase coefficient matrix is assumed as it is NOT STAR IRS ###
+    phi_IB_value = [math.sqrt(zeta) * math.exp(theta) for zeta, theta in zip(zeta_IB, theta_IB)]
+
+    ### Generate a complex matrix ###
+    phi_IB = np.zeros((N, N), dtype=complex)
+    np.fill_diagonal(phi_IB, phi_IB_value)
+
+    ### Calculate Channel Gain ###
+    H_1_B = np.dot(np.dot(np.conjugate(h_IB_B).transpose(), phi_IB), h_1_IB.transpose())
+    H_2_B = np.dot(np.dot(np.conjugate(h_IB_B).transpose(), phi_IB), h_2_IB.transpose())
+    H_11_1 = np.dot(np.dot(np.conjugate(h_I1_1).transpose(), phi_I1), h_11_I1.transpose())
+    H_12_1 = np.dot(np.dot(np.conjugate(h_I1_1).transpose(), phi_I1), h_12_I1.transpose())
+    H_21_2 = np.dot(np.dot(np.conjugate(h_I2_2).transpose(), phi_I2), h_21_I2.transpose())
+    H_22_2 = np.dot(np.dot(np.conjugate(h_I2_2).transpose(), phi_I2), h_22_I2.transpose())
+
+    ### Interference Link Channel Gain ###
+    H_2_1 = np.dot(np.dot(np.conjugate(h_IB_1).transpose(), phi_IB), h_2_IB.transpose())
+    H_1_2 = np.dot(np.dot(np.conjugate(h_IB_2).transpose(), phi_IB), h_1_IB.transpose())
+    H_22_1 = np.dot(np.dot(np.conjugate(h_I2_1).transpose(), phi_I2), h_22_I2.transpose())
+    H_21_1 = np.dot(np.dot(np.conjugate(h_I2_1).transpose(), phi_I2), h_21_I2.transpose())
+    H_12_2 = np.dot(np.dot(np.conjugate(h_I1_2).transpose(), phi_I1), h_12_I1.transpose())
+    H_11_2 = np.dot(np.dot(np.conjugate(h_I1_2).transpose(), phi_I1), h_11_I1.transpose())
+
+    ### Calculate SINR Values ###
+
+    F_1 = (P_21 * np.abs(H_22_1.item()) ** 2) + (
+                (P_dash_21 + P_dash_22) * np.abs(H_2_1.item()) ** 2 + np.abs(h_1_1.item()) ** 2 * rho ** 2 * (
+                    P_dash_11 + P_dash_12))
+
+    # SINR at R1 to detect symbol of D11 considering D12 as interference assuming H11_1 > H12_1
+    SINR_R1_D11 = (P_11 * np.abs(H_11_1.item()) ** 2) / (P_12 * np.abs(H_12_1.item()) ** 2 + F_1 + sigma_1 ** 2)
+
+    # SINR at R1 to detect symbol of D12 considering D11 as interference assuming D11 has already been decoded
+    SINR_R1_D12 = (P_12 * np.abs(H_12_1.item()) ** 2) / (F_1 + sigma_1 ** 2)
+
+    # SINR at R2
+    F_2 = P_11 * np.abs(H_11_2.item()) ** 2 + P_12 * np.abs(H_12_2.item()) ** 2 + (P_dash_11 + P_dash_12) * np.abs(
+        H_1_2.item()) ** 2 + np.abs(h_2_2.item()) ** 2 * rho ** 2 * (P_dash_21 + P_dash_12)
+
+    SINR_R2_D21 = P_21 * np.abs(H_21_2.item()) ** 2 / (P_22 * np.abs(H_22_2.item()) ** 2 + F_2 + sigma_2 ** 2)
+    SINR_R2_D22 = P_22 * np.abs(H_22_2.item()) ** 2 / (F_2 + sigma_2 ** 2)
+
+    # SINR at Base Station B
+    SINR_B_D11 = P_dash_11 * np.abs(H_1_B.item()) ** 2 / (
+                P_dash_12 * np.abs(H_1_B.item()) ** 2 + (P_dash_21 + P_dash_22) * np.abs(
+            H_2_B.item()) ** 2 + sigma_b ** 2)
+    SINR_B_D12 = P_dash_12 * np.abs(H_1_B.item()) ** 2 / (
+                (P_dash_21 + P_dash_22) * np.abs(H_2_B.item()) ** 2 + sigma_b ** 2)
+    SINR_B_D21 = P_dash_21 * np.abs(H_2_B.item()) ** 2 / (P_dash_22) * np.abs(H_2_B.item()) ** 2 + sigma_b ** 2
+    SINR_B_D22 = P_dash_22 * np.abs(H_2_B.item()) ** 2 / sigma_b ** 2
+    ### Get SINR ###
+    ### Rate of D11 ###
+    rate_D11 = math.log2(1 + find_min(SINR_R1_D11, SINR_B_D11))
+    ### Rate of D12 ###
+    rate_D12 = math.log2(1 + find_min(SINR_R1_D12, SINR_B_D12))
+    ### Rate of D21 ###
+    rate_D21 = math.log2(1 + find_min(SINR_R2_D21, SINR_B_D21))
+    ### Rate of D22 ###
+    rate_D22 = math.log2(1 + find_min(SINR_R2_D22, SINR_B_D22))
+
+
+
+
+
+
 
 def conditions(para):
+    global zeta_I1, zeta_I2, zeta_IB, theta_I1, theta_I2, theta_IB, P_11, P_12, P_21, P_22, P_dash_11, P_dash_12, P_dash_21, P_dash_22
     zeta_I1, zeta_I2, zeta_IB, theta_I1, theta_I2, theta_IB, P_11, P_12, P_21, P_22, P_dash_11, P_dash_12, P_dash_21, P_dash_22 = para
+    reinit()
     #Recalculate rates based on these parameters
     condition_1 = (rate_D11 >= R_11_th) and (rate_D12 >= R_12_th) and (rate_D21 >= R_21_th) and (rate_D22 >= R_22_th)
     ### Test absolute value of channels ###
@@ -332,12 +417,13 @@ def conditions(para):
     condition_5 = P_11 <= P_11_max and P_22 <= P_22_max and P_21 <= P_21_max and P_12 <= P_12_max
     condition_6 = P_dash_11 <= P_dash_11_max and P_dash_22 <= P_dash_22_max and P_21 <= P_dash_21_max and P_dash_12 <= P_dash_12_max
     if (condition_1 and condition_2 and condition_3 and condition_4 and condition_5 and condition_6):
-    # if (condition_1   and condition_2 and condition_5 and condition_6):
         return True
     return False
 
 def objective_function(para):
+   global zeta_I1, zeta_I2, zeta_IB, theta_I1, theta_I2, theta_IB, P_11, P_12, P_21, P_22, P_dash_11, P_dash_12, P_dash_21, P_dash_22
    zeta_I1, zeta_I2, zeta_IB, theta_I1, theta_I2, theta_IB, P_11, P_12, P_21, P_22, P_dash_11, P_dash_12, P_dash_21, P_dash_22 = para
+   reinit()
    ## Recalculate rates based on these values.
    return rate_D11 + rate_D11 + rate_D21 +  rate_D22
 
